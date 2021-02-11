482cc5b6a89252956022c9bb0efe36493eba817b
Lori Winston <Lcwinston@gmail.com>
2021-02-10 17:16:04 -0800

    added other types

--- src/App.js
+++ src/App.js
@@ -6,23 +6,16 @@ import ImageList from './components/ImageList';
 
 export default class App extends React.Component {
 state={
-  keyword: ''
+  keyword: '',
+  horns: ''
 }
-
-handleNameChange = (e) => {
-  this.setState({ 
-    title: 
-    e.target.value 
-   });
-}
-
 
 render(){
   const filteredImages=data.filter((booger) => {
   if (!this.state.keyword) return true; 
-    
-          if (booger.keyword === this.state.keyword) return true; 
-    
+  if (!this.state.horns) return true; 
+          if (booger.keyword === this.state.keyword && booger.horns === Number(this.state.horns)) return true; 
+
          return false;
   })
 
@@ -38,15 +31,39 @@ render(){
                 this.setState({
                   keyword: e.target.value
                 })
-                console.log(this.state)
               }}
 >
           <option value='rhino'>rhino</option>
-              <option value='narwhal'>narwhal</option>
+          <option value='narwhal'>narwhal</option>
+          <option value='unicorn'>unicorn</option>
+          <option value='unilego'>unilego</option>
+          <option value='triceratops'>triceratops</option>
+          <option value='markhor'>markhor</option>
+          <option value='mouflon'>rhino</option>
+          <option value='chameleon'>chameleon</option>
+          <option value='lizard'>lizard</option>
+          <option value='dragon'>dragon</option>
+
           </select>
-          <button>Submit</button>
+        </form>
+
+        <form>
+          <select
+              value={this.state.horns}
+              onChange={(e) => {
+                this.setState({
+                  horns: e.target.value
+                })
+              }}
+>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+          <option value='100'>100</option>
+          </select>
         </form>
       < ImageList ImageItemProp={ filteredImages }/>
+       
       
       
     </div>
